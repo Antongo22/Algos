3e2d054c7d0c22c754a0d231983c04fb886347ae
Anton <80984465+Antongo22@users.noreply.github.com>
2024-12-04 18:30:29 +0300

    Two Sum II - Input Array Is Sorted

---
+++ pointers/Two Sum II - Input Array Is Sorted/main.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        pointer_start = 0
+        pointer_end = len(numbers) - 1
+        
+        
+        
--- pointers/Valid Palindrome/main.py
+++ pointers/Valid Palindrome/main.py
@@ -1,22 +1,15 @@
 from typing import List
 
 class Solution:
-    def isPalindrome(self, s: str) -> bool:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
         pointer_start = 0
-        pointer_end = len(s) - 1
-                       
+        pointer_end = len(numbers) - 1
+        
+
         while pointer_start <= pointer_end:
-            if not s[pointer_start].isalnum():
-                pointer_start += 1
-                continue
-            if not s[pointer_end].isalnum():
+            if numbers[pointer_end] + numbers[pointer_start] == target:
+                return [pointer_start + 1, pointer_end + 1] 
+            elif (numbers[pointer_end] - numbers[pointer_start]) - target > 0:
                 pointer_end -= 1
-                continue
-        
-            if s[pointer_start].lower() == s[pointer_end].lower():
-                pointer_start += 1
-                pointer_end -= 1 
             else:
-                return False
-        
-        return True         
+                pointer_start += 1       
