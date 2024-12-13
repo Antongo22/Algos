15245b024a8564092f6c5571ac8aed5c21437722
Antongo22 <80984465+Antongo22@users.noreply.github.com>
2024-12-13 22:15:20 +0300

    Search in Rotated Sorted Array

--- Бинарный поиск/Search in Rotated Sorted Array/main.py
+++ Бинарный поиск/Search in Rotated Sorted Array/main.py
@@ -1,3 +1,32 @@
+from typing import List
+
+
 class Solution:
+    def findStart(self, nums: List[int]):
+        l, r = -1, len(nums) - 1
+
+        while r - l > 1:
+            m = (l + r) // 2
+
+            if nums[m] > nums[-1]:
+                l = m
+            else:
+                r = m
+
+        return r
+
     def search(self, nums: List[int], target: int) -> int:
-        
+        os = self.findStart(nums)
+        l, r = os, len(nums) + os
+
+        while r - l > 1:
+            m = (l + r) // 2
+
+            if nums[m % len(nums)] <= target:
+                l = m
+            else:
+                r = m
+
+        realLeft = l % len(nums)
+        return realLeft if nums[realLeft] == target else -1
+
