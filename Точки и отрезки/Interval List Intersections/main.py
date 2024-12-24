11b820612370837388c90b0738520663d8ba5ed6
Antongo22 <80984465+Antongo22@users.noreply.github.com>
2024-12-24 23:22:04 +0300

    Пересечения списка интервалов

---
+++ Точки и отрезки/Interval List Intersections/main.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
+        p1, p2 = 0, 0
+        res = []
+        while p1 < len(firstList) and p2 < len(secondList):
+            if max(firstList[p1][0], secondList[p2][0]) <= min(firstList[p1][1], secondList[p2][1]):
+                res.append([ max(firstList[p1][0], secondList[p2][0]), min(firstList[p1][1], secondList[p2][1])])
+
+            if firstList[p1][1] < secondList[p2][1]:
+                p1 += 1
+            else:
+                p2 += 1
+        return res
